5653093cb0107e4f8f92f6cb6767fa1434c03b5d
Dave Swartz <dwswartz@gmail.com>
2013-09-02 12:12:05 -0400

    Label placement is no longer done using force layout. All elements are resized when the view is zoomed in/out. Subway lines are animated. Coast outline was removed. The feedback page was removed.

--- public/javascripts/mapperly.js
+++ public/javascripts/mapperly.js
@@ -14,6 +14,20 @@
  * limitations under the License.
  * ========================================================== */
 
+var projection = d3.geo.transverseMercator()
+    .rotate([73.96, -40.69])
+	.scale(1000000); 
+
+var path = d3.geo.path().projection(projection);
+
+var zoom = d3.behavior.zoom()
+	.scaleExtent([0.25, 4])
+	.on("zoom", zoommove)
+
+var map = d3.select("#vis")
+	.append("svg")
+   	.call(zoom);
+
 var lineToColors = d3.map({ 
 	"1" : "#EF3E42",
 	"2" : "#EF3E42",
@@ -39,208 +53,25 @@ var lineToColors = d3.map({
 	"S" : "#6C6D70",
 	"SIR" : "#1D70B3"
 });
-
-function isPointVisible(coordinates) {
-	var p = projection(coordinates);
-	return p[0] > -20 && p[0] < width + 20 && p[1] > -20 && p[1] < height + 20;
-}
-
-function showLabel(d) {
-	return isPointVisible(d.geometry.coordinates); 
-}
-
-var originJson = {
-    lat : 40.707944,
-    lon : -74.003683
-};
-  
-var upperLeftJson = {
-    lat : 40.908,
-    lon : -74.261
-};
-
-var lowerRightJson = {
-    lat : 40.490,
-    lon : -73.745
-};
-
-var initialScale = 800000;
-
-var minScaleFactor = 0.5;
-var maxScaleFactor = 4;
-
-var width = 1130;
-var height = 550;
-
-var projection = d3.geo.albers().origin([originJson.lon, originJson.lat]).scale(initialScale).translate([width/4, height*5/8]);
-
-var s = projection.scale();
-var path = d3.geo.path().projection(projection);
-path.pointRadius(6);
-var zoom = d3.behavior.zoom()
-	.translate(projection.translate())
-	.scaleExtent([minScaleFactor, maxScaleFactor])
-	.on("zoom", panzoom);
-var map = d3.select("#vis").append("svg:svg")
-	.call(zoom);
-var coast = map.append("svg:g").attr("id", "coast");
 var lines = map.append("svg:g").attr("id", "line");
-var transfers = map.append("svg:g").attr("id", "transfer");
-var stops = map.append("svg:g").attr("id", "stop");
-
-// Force directed layout (http://bl.ocks.org/1095795)
-var force = d3.layout.force()
-	.linkStrength(10)
-	.linkDistance(10)
-	.charge(-1000)
-	.gravity(0)
-	.friction(0.3)
-	.size([width, height]);
-
-var nodes = force.nodes();
-var links = force.links();
-
-force.on("tick", function() {
-	links.forEach(function(d) { // TODO <<<< HERE, should only need to do this on panzoom (not each tick)
-		var sourceScreen = projection(d.source.coords);
-		d.source.x = sourceScreen[0];
-		d.source.y = sourceScreen[1];
-		d.target.offset = [d.target.x - d.source.x, d.target.y - d.source.y];
-	});
-
-	map.selectAll("g.node")
-		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	
-	map.selectAll("line.link")
-		.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; });
-});
-
-function restart() { // TODO if I can pass in nodes and links can change updateNodes to be functional
-	var link = map.selectAll("line.link")
-		.data(links);
-	
-	link.enter().insert("svg:line", "g.node")
-		.attr("class", "link");
-	
-	link.exit().remove();
-	
-	var node = map.selectAll("g.node")
-		.data(nodes, function(d) { return d.name }); // Needed or else label text changes
-	
-	var nodeEnter = node.enter().append("svg:g")
-		.attr("class", "node");
-	
-	nodeEnter.append("svg:text")
-		.text(function(d) { return d.name }) 
-		.attr("dy", 6)	
-		.attr("id", "backer");	
-	nodeEnter.append("svg:text")
-		.text(function(d) { return d.name }) 
-		.attr("dy", 6)	
-		.attr("id", "label");
-	
-	node.exit().remove();
-
-	nodes.filter(function(n) { return !n.fixed }).forEach(function(d) {
-		var sourceScreen = projection(d.coords);
-		d.x = sourceScreen[0] + d.offset[0];
-		d.y = sourceScreen[1] + d.offset[1];
-	});
-	
-	force.start();
-}
-
-function containsCoords(nodes, coords) {
-	return nodes.some(function(n, i) { 
-		return coords[0] == n.coords[0] && coords[1] == n.coords[1] 
-	});
-}
-
-function updateNodes() { 
-	// Note: modify existing arrays as reference is kept in the force-layout
-
-	// Updates list of visible nodes
-	var updatedNodes = stopsJson.features.filter(showLabel)
-		.map(function(n) { 
-			n.coords = n.geometry.coordinates;
-			return n;
-		});
-
-	// Removes nodes no longer visible
-	nodes
-		.map(function(n, i) {
-			return !containsCoords(updatedNodes, n.coords) ? i : undefined; 
-		}) 
-	    .filter(function(i) {
-			return !_.isUndefined(i); 
-		})
-		.reverse()
-	    .forEach(function(i) { 
-			nodes.splice(i,1) 
-		});
-
-	// Removes links for nodes which are no longer visible
-	links
-		.map(function(l, i) {
-			return !containsCoords(updatedNodes, l.source.coords) ? i : undefined; 
-		})
-	    .filter(function(i) {
-			return !_.isUndefined(i); 
-		})
-		.reverse()
-	    .forEach(function(i) { 
-			links.splice(i,1) 
-		});
-
-	// Adds nodes and links which become visible
-	var toAdd = updatedNodes
-		.filter(function(n) { return !containsCoords(nodes, n.coords) })
-		.forEach(function(n) { 
-			// Get current screen position of stop
-			var screen = projection(n.coords);
-
-			// Adds jitter to initial label position avoid initial label bounce
-			var xJitter = Math.random()*10;
-			var yJitter = Math.sqrt(100 - xJitter*xJitter);
-			n.x = screen[0] + xJitter;
-			n.y = screen[1] + yJitter;
-			n.offset = [xJitter, yJitter];
-
-			// Adds node for floating label
-			nodes.push(n); 
-
-			// Adds node for fixed stop
-			var f = {x: screen[0], y: screen[1], fixed: true, coords: n.coords};
-			nodes.push(f);
-
-			// Adds link between stop and label
-			links.push({source: f, target: n});
-		});
-
-	restart();
-}
-
-d3.json("assets/data/coasts.json", function(data) {
-	coast.selectAll("coast")
-		.data(data.features)
-		.enter().append("svg:path")
-		.attr("d", path);
-});
-
 d3.json("assets/data/paths.json", function(data) {
 	lines.selectAll("line")
 		.data(data.features)
 		.enter().append("svg:path")
-		.attr("stroke", function(d, i) { 
-			return d3.rgb(lineToColors.get(d.lines[0])) 
-		})
+		.attr("stroke", function(d, i) { return d3.rgb(lineToColors.get(d.properties.lines[0])); })
+		.attr("stroke-dasharray", "20,5,20,5,20,5,20,5,20,5,20,5,20,5,20,100")
+		.attr("stroke-dashoffset", 0)
+		.transition()
+            .duration(240000)
+            .ease("linear")
+            .attr("stroke-dashoffset", 10000)
 		.attr("d", path);
-//		.each(createTransition());
+
+    path
+      
 });
 
+var transfers = map.append("svg:g").attr("id", "transfer");
 d3.json("assets/data/transfers.json", function(data) {
 	transfers.selectAll("transfer")
 		.data(data.features)
@@ -248,99 +79,26 @@ d3.json("assets/data/transfers.json", function(data) {
 		.attr("d", path);
 });
 
-
+var stops = map.append("svg:g").attr("id", "stop");
 d3.json("assets/data/stops.json", function(data) {
-	stopsJson = data;
 	stops.selectAll("stop")
 		.data(data.features)
 		.enter().append("svg:path")
 		.attr("d", path);
-	updateNodes();
+	stops.selectAll("stop")
+		.data(data.features)
+	    .enter().append("svg:text")
+		.text(function(d) { return d.properties.name; }) 
+		.attr("dy", 6)
+		.attr("id", "label")
+		.attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+		.attr("x", function(d) { return 6; })
+		.attr("y", function(d) { return -6; })
+		.style("text-anchor", function(d) { return "start"; });
 });
 
-function panzoom() {
-	// Updates the projection ignoring translation bounds
-	projection.translate(d3.event.translate);
-	projection.scale(s * d3.event.scale);
-	panzoomed(d3.event.scale, d3.event.translate);
+function zoommove() {
+  lines.attr("transform", "translate("+d3.event.translate+")"+" scale("+d3.event.scale+")");
+  transfers.attr("transform", "translate("+d3.event.translate+")"+" scale("+d3.event.scale+")");
+  stops.attr("transform", "translate("+d3.event.translate+")"+" scale("+d3.event.scale+")");
 }
-
-function panzoomed(scale, t) {
-	// Computes the translation bounds
-	var upperLeft = projection([upperLeftJson.lon, upperLeftJson.lat]);
-	var lowerRight = projection([lowerRightJson.lon, lowerRightJson.lat]);
-	if (upperLeft[0] > 0) {
-		t[0] -= upperLeft[0];
-	}
-	if (upperLeft[1] > 0) {
-		t[1] -= upperLeft[1];
-	}
-	if (lowerRight[0] < width) {
-		t[0] -= (lowerRight[0] - width);
-	}
-	if (lowerRight[1] < height) {
-		t[1] -= (lowerRight[1] - height);
-	}
-
-	// Updates the projection and zoom accounting for translation bounds
-	projection.translate(t);
-	projection.scale(s * scale);
-
-	zoom.translate(t).scale(scale);
-
-	updateNodes();
-
-	coast.selectAll("path").attr("d", path);
-	lines.selectAll("path").attr("d", path);
-	transfers.selectAll("path").attr("d", path);
-	stops.selectAll("path").attr("d", path);
-}
-
-function createTransition() {
-	return function(d, i) {
-		var gs = _.sortBy(_.uniq(d.routes, function(r) { return r.color }), function(r) { return r.index; });
-		if (gs.length > 1) {
-			d3.select(this).transition()
-				.delay()
-				.duration(gs[0].index * 2000)
-				.attr("stroke", d3.rgb(gs[0].color))
-				.each("end", transition(gs, 1));
-		}
-	};
-}
-
-function transition(gs, i) {
-	return function() {				  
-		var delay = gs[i].index - gs[i == 0 ? gs.length -1 : i - 1].index;
-		if (delay < 0) delay += 6
-		d3.select(this).transition()
-			.delay(500)
-			.duration(delay * 2000 - 500)
-			.attr("stroke", d3.rgb(gs[i].color))
-			.each("end", transition(gs, i == gs.length - 1 ? 0 : i + 1));
-	};
-}
-
-/* UI button functions */
-
-function doZoom(multiple) {
-	var zoomFactor = Math.min(maxScaleFactor, zoom.scale() * multiple);
-	var pan = [width/2 + zoomFactor/zoom.scale()*(zoom.translate()[0] - width/2), height/2 + zoomFactor/zoom.scale()*(zoom.translate()[1] - height/2)];
-	projection.scale(s * zoomFactor);
-	projection.translate(pan);
-	panzoomed(zoomFactor, pan);
-}
-
-function zoomIn() { doZoom(2); }
-function zoomOut() { doZoom(0.5); }
-
-function doPan(p) {
-	var pan = [zoom.translate()[0] + p[0], zoom.translate()[1] + p[1]];
-	projection.translate(pan);
-	panzoomed(zoom.scale(), pan);
-}
-
-function panLeft() { doPan([width/4, 0]); }
-function panRight() { doPan([-width/4, 0]); }
-function panUp() { doPan([0, height/4]); }
-function panDown() { doPan([0, -height/4]); }
